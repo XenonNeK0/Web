7da0445de587b1e4bd66fff4fcf26262f1b7557f
XenonNek0 <xenonnek0@163.com>
2025-01-30 11:58:05 +0800

    模板设置

--- FlaskWeb2/app.py
+++ FlaskWeb2/app.py
@@ -24,5 +24,15 @@ def revision(revNo):
     return "Revision Number %f" % revNo
 
 
+@app.route("/flask")
+def hello_flask():
+    return "Hello flask"
+
+
+@app.route("/python/")
+def hello_python():
+    return "Hello python"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
